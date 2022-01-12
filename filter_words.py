65adc9699ce0727d13328ebbc50565e4b5e8a08f
Cihad <cihad@Cihads-Air.fritz.box>
2022-01-12 22:18:10 +0100

    add new list

--- filter_words.py
+++ filter_words.py
@@ -8,4 +8,4 @@ with open("5char_words.txt", "w") as file:
         # überprüfen ob nicht kaputt und länge 5
         if (len(word) == 6 and "�" not in word and word[0].isupper()):
             # wenn sauber, schreibe in neue datei
-            file.write(word)
+            file.write(word.lower())
